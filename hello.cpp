5571f21e9390d595549b5d32b95e2b70a822503b
Lynn <109832568+TuxMC-sys@users.noreply.github.com>
2024-02-02 20:35:30 -0500

    Added name input

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-
+#include<iostream>
+#include<string>
+using namespace std;
 int main()
 {
-	std::cout << "Hello, world!\n";
+	cout << "Please enter your first name: ";
+	string first_name;
+	cin >> first_name;
+	cout << "Hello " << first_name << "!\n";
 	return 0;
 }
