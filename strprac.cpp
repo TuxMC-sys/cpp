3bc43ba0bd3e280ddd0b62453a4db5432b0f60be
Lynn <109832568+TuxMC-sys@users.noreply.github.com>
2024-02-03 08:48:53 -0500

    added func

--- strprac.cpp
+++ strprac.cpp
@@ -4,13 +4,15 @@
 using namespace std;
 
 int main() {
+    int num_of_words = 0;
     cout << "Enter a phrase to check for repeated words \n";
     string previous = " "; // previous word; initialized to “not a word”
     string current; // current word
 
     while (cin>>current) { // read a stream of words
+        ++num_of_words;
         if (previous == current) // check if the word is the same as last
-            cout << "repeated word: " << current << '\n';
+            cout << "Word Number " << num_of_words << " repeated word: " << current << '\n';
     previous = current;
     }
 }
