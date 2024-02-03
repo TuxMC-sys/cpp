8ccfa9166ac08e13d6fe3cd8a5c094dbaa4b1f7f
Lynn <109832568+TuxMC-sys@users.noreply.github.com>
2024-02-03 17:28:39 -0500

    Constant

--- mk.cpp
+++ mk.cpp
@@ -3,9 +3,10 @@
 #include<string>
 
 using namespace std;
-
+constexpr double kmmir = 1.60934;
+constexpr double mikmr = 0.621371;
 int main() {
-    cout << "This program converts from Miles to Kilometers and vice-versa";
+    cout << "This program converts from Miles to Kilometers and vice-versa\n";
     char conv;
     cout << "Are you converting from Miles to Kilometers, please enter \"m\" for miles or \"k\" for kilometers: ";
     cin >> conv;
@@ -13,14 +14,14 @@ int main() {
         cout << "Please enter the number of miles to be converted: ";
         double mi {0};
         cin >> mi;
-        double km = mi * 0.621371;
+        double km = mi * mikmr;
         cout << mi << " Miles is " << km << " Kilometers.\n";
     }
     else if (conv == 'k') {
         cout << "Please enter the number of kilometers to be converted: ";
         double km {0};
         cin >> km;
-        double mi = km * 1.60934;
+        double mi = km * kmmir;
         cout << km << " Kilometers is " << mi << " Miles.\n";
     }
     else {
