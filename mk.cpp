1b3c3b12a44715e1b605d17e61cc3d65059904b1
Lynn <109832568+TuxMC-sys@users.noreply.github.com>
2024-02-03 17:30:19 -0500

    removed unneeded line

--- mk.cpp
+++ mk.cpp
@@ -1,31 +1,29 @@
 #include<iostream>
-#include<cmath>
-#include<string>
 
-using namespace std;
+
 constexpr double kmmir = 1.60934;
 constexpr double mikmr = 0.621371;
 int main() {
-    cout << "This program converts from Miles to Kilometers and vice-versa\n";
+    std::cout << "This program converts from Miles to Kilometers and vice-versa\n";
     char conv;
-    cout << "Are you converting from Miles to Kilometers, please enter \"m\" for miles or \"k\" for kilometers: ";
-    cin >> conv;
+    std::cout << "Are you converting from Miles to Kilometers, please enter \"m\" for miles or \"k\" for kilometers: ";
+    std::cin >> conv;
     if (conv == 'm') {
-        cout << "Please enter the number of miles to be converted: ";
+        std::cout << "Please enter the number of miles to be converted: ";
         double mi {0};
-        cin >> mi;
+        std::cin >> mi;
         double km = mi * mikmr;
-        cout << mi << " Miles is " << km << " Kilometers.\n";
+        std::cout << mi << " Miles is " << km << " Kilometers.\n";
     }
     else if (conv == 'k') {
-        cout << "Please enter the number of kilometers to be converted: ";
+        std::cout << "Please enter the number of kilometers to be converted: ";
         double km {0};
-        cin >> km;
+        std::cin >> km;
         double mi = km * kmmir;
-        cout << km << " Kilometers is " << mi << " Miles.\n";
+        std::cout << km << " Kilometers is " << mi << " Miles.\n";
     }
     else {
-        cout << "Please re-run the program with a valid input\n";
+        std::cout << "Please re-run the program with a valid input\n";
     }
     return 0;
 }
